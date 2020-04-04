b146fda1dfba22f0f8e0baf9aec7440baded704e
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-04-04 21:53:22 +0700

    Prettier

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,5 @@
 const yargs = require('yargs');
 
-const { argv } = yargs
-  .usage('$0 [string]')
-  .alias('v', 'version')
-  .version()
-  .help();
+const { argv } = yargs.usage('$0 [string]').alias('v', 'version').version().help();
 
 module.exports = argv;
